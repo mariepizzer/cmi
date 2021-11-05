f1df9f53d1122ef275005cbb9d8d373baa0bf11c
Marie <gozzpi28@gmail.com>
2021-11-05 14:42:33 -0500

    send message public

--- wwwroot/Content/assets/js/custom/chat.js
+++ wwwroot/Content/assets/js/custom/chat.js
@@ -7,6 +7,59 @@ var __webpack_exports__ = {};
 
 
 // Class definition
+
+function sendMessage  (messages, input, messageObj) {
+	if (input.value.length === 0) {
+		return;
+	}
+
+	var messageOutTemplate = messages.querySelector('[data-kt-element="template-out"]');
+	var messageTemplate = messageOutTemplate.cloneNode(true);
+	messageTemplate.classList.remove('d-none');
+	messageTemplate.querySelector('[data-kt-element="message-text"]').innerText = input.value;
+	messageTemplate.querySelector('[data-kt-element="out-contact-avatar"]').innerText = messageObj.name.charAt(0);
+	messageTemplate.querySelector('[data-kt-element="out-contact-name"]').innerText = messageObj.name;
+	messageTemplate.querySelector('[data-kt-element="out-time"]').innerText = messageObj.time;
+	input.value = '';
+	messages.appendChild(messageTemplate);
+	messages.scrollTop = messages.scrollHeight;
+}
+
+function receiveMessage (messages, messageObj) {
+	var messageInTemplate = messages.querySelector('[data-kt-element="template-in"]');
+	var messageTemplate = messageInTemplate.cloneNode(true);
+	messageTemplate.classList.remove('d-none');
+	messageTemplate.querySelector('[data-kt-element="message-text"]').innerText = messageObj.text;
+	messageTemplate.querySelector('[data-kt-element="in-contact-avatar"]').innerText = messageObj.name.charAt(0);
+	messageTemplate.querySelector('[data-kt-element="in-contact-name"]').innerText = messageObj.name;
+	messageTemplate.querySelector('[data-kt-element="in-time"]').innerText = messageObj.time;
+	messages.appendChild(messageTemplate);
+	messages.scrollTop = messages.scrollHeight;
+}
+
+function setContact (element, contactData) {
+	var contacts = element.querySelector('[data-kt-element="contacts"]');
+	var contactsTemplate = contacts.querySelector('[data-kt-element="contact-template"]');
+	var contact = contactsTemplate.cloneNode(true);
+	contact.classList.remove('d-none');
+	contact.querySelector('[data-kt-element="contact-avatar"]').innerText = contactData.name.charAt(0);
+	contact.querySelector('[data-kt-element="contact-email"]').innerText = contactData.email;
+	contact.querySelector('[data-kt-element="contact-name"]').innerText = contactData.name;
+	contacts.appendChild(contact);
+	contacts.scrollTop = contacts.scrollHeight;
+}
+
+function setAvatar (avatarsElement, contactData) {
+	var avatars = avatarsElement.querySelector('[data-kt-element="contact-avatars"]');
+	var avatarsTemplate = avatars.querySelector('[data-kt-element="contact-avatar-template"]');
+	var avatar = avatarsTemplate.cloneNode(true);
+	avatar.classList.remove('d-none');
+	avatar.querySelector('[data-kt-element="contact-initial"]').innerText = contactData.name.charAt(0);
+	avatars.appendChild(avatar);
+	avatars.scrollTop = avatars.scrollHeight;
+}
+
+
 var KTAppChat = function () {
 	// Private functions
 	var handlerSend = function (messengerElement) {
@@ -40,70 +93,7 @@ var KTAppChat = function () {
 			sendMessage(messages, input, outgoingMessageObj);
 		});
 	}
-	var handlerMessaging = function (element) {
-		sendMessage(messages, input, outgoingMessageObj);
-
-		setTimeout(function () {
-			let incomingMessageObj = {
-				text: "Magic reply rest here!",
-				name: "Jaime",
-				time: "Just now"
-			}
-			receiveMessage(messages, incomingMessageObj);
-		}, 500);
-	}
-
-	var sendMessage = function (messages, input, messageObj) {
-		if (input.value.length === 0) {
-			return;
-		}
-
-		var messageOutTemplate = messages.querySelector('[data-kt-element="template-out"]');
-		var messageTemplate = messageOutTemplate.cloneNode(true);
-		messageTemplate.classList.remove('d-none');
-		messageTemplate.querySelector('[data-kt-element="message-text"]').innerText = input.value;
-		messageTemplate.querySelector('[data-kt-element="out-contact-avatar"]').innerText = messageObj.name.charAt(0);
-		messageTemplate.querySelector('[data-kt-element="out-contact-name"]').innerText = messageObj.name;
-		messageTemplate.querySelector('[data-kt-element="out-time"]').innerText = messageObj.time;
-		input.value = '';
-		messages.appendChild(messageTemplate);
-		messages.scrollTop = messages.scrollHeight;
-	}
-
-	var receiveMessage = function (messages, messageObj) {
-		var messageInTemplate = messages.querySelector('[data-kt-element="template-in"]');
-		var messageTemplate = messageInTemplate.cloneNode(true);
-		messageTemplate.classList.remove('d-none');
-		messageTemplate.querySelector('[data-kt-element="message-text"]').innerText = messageObj.text;
-		messageTemplate.querySelector('[data-kt-element="in-contact-avatar"]').innerText = messageObj.name.charAt(0);
-		messageTemplate.querySelector('[data-kt-element="in-contact-name"]').innerText = messageObj.name;
-		messageTemplate.querySelector('[data-kt-element="in-time"]').innerText = messageObj.time;
-		messages.appendChild(messageTemplate);
-		messages.scrollTop = messages.scrollHeight;
-	}
-
-	var handleContact = function (element, contactData) {
-		var contacts = element.querySelector('[data-kt-element="contacts"]');
-		var contactsTemplate = contacts.querySelector('[data-kt-element="contact-template"]');
-		var contact = contactsTemplate.cloneNode(true);
-		contact.classList.remove('d-none');
-		contact.querySelector('[data-kt-element="contact-avatar"]').innerText = contactData.name.charAt(0);
-		contact.querySelector('[data-kt-element="contact-email"]').innerText = contactData.email;
-		contact.querySelector('[data-kt-element="contact-name"]').innerText = contactData.name;
-		contacts.appendChild(contact);
-		contacts.scrollTop = contacts.scrollHeight;
-	}
-
-	var handleAvatar = function (avatarsElement, contactData) {
-		var avatars = avatarsElement.querySelector('[data-kt-element="contact-avatars"]');
-		var avatarsTemplate = avatars.querySelector('[data-kt-element="contact-avatar-template"]');
-		var avatar = avatarsTemplate.cloneNode(true);
-		avatar.classList.remove('d-none');
-		avatar.querySelector('[data-kt-element="contact-initial"]').innerText = contactData.name.charAt(0);
-		avatars.appendChild(avatar);
-		avatars.scrollTop = avatars.scrollHeight;
-	}
-
+	
 	// Public methods
 	return {
 		init: function (messengerElement, contactsElement, avatarsElement) {
@@ -115,8 +105,8 @@ var KTAppChat = function () {
 			];
 			handlerSend(messengerElement);
 			contactsData.map(function (contactData) {
-				handleContact(contactsElement, contactData);
-				handleAvatar(avatarsElement, contactData);
+				setContact(contactsElement, contactData);
+				setAvatar(avatarsElement, contactData);
 			})
 		}
 	};
